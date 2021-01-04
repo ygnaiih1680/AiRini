05a1dc8a3ccb7dd51273478278d048dac47cf2b6
AiRini <ygnaiih1680@mju.ac.kr>
2021-01-04 19:34:11 +0900

    change html

--- src/component/WindowOperationButton.jsx
+++ src/component/WindowOperationButton.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 class WindowOperationButton extends Component {
+    /* TODO: 2021/01/04 adds minimize maximize close */
     render() {
         return (
             <div>
